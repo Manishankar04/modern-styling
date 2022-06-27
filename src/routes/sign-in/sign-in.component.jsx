d8fbabe83bee933d4d353b6be9ff9746a33b22be
Manishankar04 <manishankarsridhar@gmail.com>
2022-06-27 15:05:41 +0530

    Updated few components in signin and signup

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,21 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+
+import {
+        signInWithGooglePopup, 
+        signInWithGoogleRedirect, 
+        createUserDocumentFromAuth
+        } from '../../utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 const SignIn = ()=>{
     const logGoogleUser =async()=>{
         const {user} = await signInWithGooglePopup();
-        const userDocref= await  createUserDocumentFromAuth(user);
+        const userDocref= await createUserDocumentFromAuth(user);
     }
     return(
         <div>
                 <h1>SignIn</h1>
                 <button onClick={logGoogleUser}> Signin with google popup</button>
+                <SignUpForm />
         </div>
     );
-
 }
-
 export default SignIn;
